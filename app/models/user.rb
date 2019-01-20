3df22544df34fe213e78a7c3b97ace1927593276
cuurjol <cuurjol@gmail.com>
2019-01-20 14:55:28 +0300

    Models was edited.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :is_admin, inclusion: {in: [true, false]}, allow_nil: false
+  validates :balance, numericality: {only_integer: true}, allow_nil: false
+
+  has_many :games, dependent: :destroy
+
+  def average_prize
+    (balance.to_f/games.count).round unless games.count.zero?
+  end
 end
