233a133984a607f6ef90f9119dbc614068acd81a
cuurjol <cuurjol@gmail.com>
2019-01-21 16:46:49 +0300

    First version application.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_devise_params, if: :devise_controller?
+
+  before_action :set_new_game
+
+  def configure_devise_params
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+      user.permit(:name, :email, :password, :password_confirmation)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |user|
+      user.permit(:name, :password, :password_confirmation, :current_password)
+    end
+  end
+
+  def set_new_game
+    @new_game ||= Game.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#index'
+
+  devise_for :users
+
+  resources :users, only: [:index, :show]
+
+  resources :games, only: [:create, :show] do
+    put 'answer', on: :member
+    put 'take_money', on: :member
+  end
 end
